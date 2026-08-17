3ed0cf53890add0515a369b91e8e71e933064ac2
祈睿 <laiqirui@gmail.com>
2026-08-17 20:01:50 +0800

    新增個人化歡迎訊息

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const messageInput = document.querySelector("#message");
 const result = document.querySelector("#form-result");
 
 button.addEventListener("click", () => {
-  const userName = nameInput.value.trim().toUpperCase();
+  const userName = nameInput.value.trim().toLowerCase();
   const message = messageInput.value.trim();
 
  if (userName === "" || message === "") {
     result.textContent = "請先填寫名字和訊息！";
   } else {
-   result.textContent = "嗨，" + userName + "！歡迎來到我的網站！"; 
-
+    if (userName === "ray") {
+        result.textContent = "嗨，ray！歡迎回來！";
+    } else {
+        result.textContent = "嗨，" + userName + "！歡迎來到我的網站！";
+    }
     nameInput.value = "";
     messageInput.value = "";
   }
