207752e7b8df223f0b76bf22b5c27eed85390515
祈睿 <laiqirui@gmail.com>
2026-08-17 19:40:47 +0800

    更新名字輸入功能

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const messageInput = document.querySelector("#message");
 const result = document.querySelector("#form-result");
 
 button.addEventListener("click", () => {
-  const name = nameInput.value.trim();
+  const userName = nameInput.value.trim().toUpperCase();
   const message = messageInput.value.trim();
 
-  if (name === "" || message === "") {
+ if (userName === "" || message === "") {
     result.textContent = "請先填寫名字和訊息！";
   } else {
-    result.textContent = "謝謝你，" + name + "！我已收到你的訊息。";
+   result.textContent = "嗨，" + userName + "！歡迎來到我的網站！"; 
 
     nameInput.value = "";
     messageInput.value = "";
